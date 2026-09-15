e3887f71cd3e6bf5987a8a7139ac0f9c3fcb71a0
RAMANABORA <rb2945611@gmail.com>
2026-09-15 21:11:42 +0530

    Time: 3 ms (95.88%) | Memory: 19.3 MB (72.68%) - LeetSync

--- 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.py
+++ 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.py
@@ -1,15 +1,29 @@
 class Solution:
     def maxPalindromes(self, s: str, k: int) -> int:
+        
+        def check(l, r):
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+
         n = len(s)
-        pal = [[False] * n for _ in range(n)]
-        for i in range(n - 1, -1, -1):
-            for j in range(i, n):
-                if s[i] == s[j] and (j - i <= 1 or pal[i + 1][j - 1]):
-                    pal[i][j] = True
-        dp = [0] * (n + 1)
-        for i in range(n):
-            dp[i + 1] = dp[i]
-            for l in range(i + 1):
-                if i - l + 1 >= k and pal[l][i]:
-                    dp[i + 1] = max(dp[i + 1], dp[l] + 1)
-        return dp[n]
+        ans = 0
+        start = 0
+
+        for r in range(k - 1, n):
+            l = r - k + 1
+
+            if l >= start and check(l, r):
+                ans += 1
+                start = r + 1
+                continue
+            
+            l = r - k
+            if l >= start and check(l, r):
+                ans += 1
+                start = r + 1
+
+        return ans
